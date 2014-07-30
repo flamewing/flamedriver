d3fc29c06653dbf4c3553e9dea486901105efc95
flamewing <flamewing.sonic@gmail.com>
2014-07-30 11:55:09 -0300

    Adding voice names again.

--- UniBank.asm
+++ UniBank.asm
@@ -1,6 +1,7 @@
 ; ===========================================================================
 ; FM Universial Voice Bank
 ; ===========================================================================
+; Synth Bass 2
 ;	Voice $00
 ;	$3C
 ;	$01, $00, $00, $00, 	$1F, $1F, $15, $1F, 	$11, $0D, $12, $05
@@ -19,6 +20,7 @@
 	smpsVcReleaseRate   $0A, $05, $0A, $05
 	smpsVcTotalLevel    $00, $07, $00, $1A
 
+; Trumpet 1
 ;	Voice $01
 ;	$3D
 ;	$01, $01, $01, $01, 	$94, $19, $19, $19, 	$0F, $0D, $0D, $0D
@@ -37,6 +39,7 @@
 	smpsVcReleaseRate   $0A, $0A, $0A, $05
 	smpsVcTotalLevel    $00, $00, $00, $15
 
+; Slap Bass 2
 ;	Voice $02
 ;	$03
 ;	$00, $D7, $33, $02, 	$5F, $9F, $5F, $1F, 	$13, $0F, $0A, $0A
@@ -55,6 +58,7 @@
 	smpsVcReleaseRate   $0A, $05, $05, $05
 	smpsVcTotalLevel    $00, $15, $16, $13
 
+; Synth Bass 1
 ;	Voice $03
 ;	$34
 ;	$70, $72, $31, $31, 	$1F, $1F, $1F, $1F, 	$10, $06, $06, $06
@@ -73,6 +77,7 @@
 	smpsVcReleaseRate   $0A, $05, $0A, $05
 	smpsVcTotalLevel    $03, $18, $03, $10
 
+; Bell Synth 1
 ;	Voice $04
 ;	$3E
 ;	$77, $71, $32, $31, 	$1F, $1F, $1F, $1F, 	$0D, $06, $00, $00
@@ -91,6 +96,7 @@
 	smpsVcReleaseRate   $0A, $0A, $0A, $05
 	smpsVcTotalLevel    $00, $00, $00, $1B
 
+; Bell Synth 2
 ;	Voice $05
 ;	$34
 ;	$33, $41, $7E, $74, 	$5B, $9F, $5F, $1F, 	$04, $07, $07, $08
@@ -109,6 +115,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $07, $29, $00, $23
 
+; Synth Brass 1
 ;	Voice $06
 ;	$3A
 ;	$01, $07, $31, $71, 	$8E, $8E, $8D, $53, 	$0E, $0E, $0E, $03
@@ -127,6 +134,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $27, $28, $18
 
+; Synth like Bassoon
 ;	Voice $07
 ;	$3C
 ;	$32, $32, $71, $42, 	$1F, $18, $1F, $1E, 	$07, $1F, $07, $1F
@@ -145,6 +153,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $0C, $00, $1E
 
+; Bell Horn type thing
 ;	Voice $08
 ;	$3C
 ;	$71, $72, $3F, $34, 	$8D, $52, $9F, $1F, 	$09, $00, $00, $0D
@@ -163,6 +172,7 @@
 	smpsVcReleaseRate   $07, $02, $08, $03
 	smpsVcTotalLevel    $07, $1D, $00, $15
 
+; Synth Bass 3
 ;	Voice $09
 ;	$3D
 ;	$01, $01, $00, $00, 	$8E, $52, $14, $4C, 	$08, $08, $0E, $03
@@ -181,6 +191,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $1B, $00, $00, $1B
 
+; Synth Trumpet
 ;	Voice $0A
 ;	$3A
 ;	$01, $01, $01, $02, 	$8D, $07, $07, $52, 	$09, $00, $00, $03
@@ -199,6 +210,7 @@
 	smpsVcReleaseRate   $08, $02, $02, $02
 	smpsVcTotalLevel    $00, $18, $22, $18
 
+; Wood Block
 ;	Voice $0B
 ;	$3C
 ;	$36, $31, $76, $71, 	$94, $9F, $96, $9F, 	$12, $00, $14, $0F
@@ -217,6 +229,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $1A, $00, $33
 
+; Tubular Bell
 ;	Voice $0C
 ;	$34
 ;	$33, $41, $7E, $74, 	$5B, $9F, $5F, $1F, 	$04, $07, $07, $08
@@ -235,6 +248,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $17, $29, $10, $23
 
+; Strike Bass
 ;	Voice $0D
 ;	$38
 ;	$63, $31, $31, $31, 	$10, $13, $1A, $1B, 	$0E, $00, $00, $00
@@ -253,6 +267,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $1A, $19, $1A
 
+; Elec Piano
 ;	Voice $0E
 ;	$3A
 ;	$31, $25, $73, $41, 	$5F, $1F, $1F, $9C, 	$08, $05, $04, $05
@@ -271,6 +286,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $1F, $27, $29
 
+; Bright Piano
 ;	Voice $0F
 ;	$04
 ;	$71, $41, $31, $31, 	$12, $12, $12, $12, 	$00, $00, $00, $00
@@ -289,6 +305,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $23, $00, $23
 
+; $Curch Bell
 ;	Voice $10
 ;	$14
 ;	$75, $72, $35, $32, 	$9F, $9F, $9F, $9F, 	$05, $05, $00, $0A
@@ -307,6 +324,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $14, $00, $1E
 
+; Synth Brass 2
 ;	Voice $11
 ;	$3D
 ;	$01, $00, $01, $02, 	$12, $1F, $1F, $14, 	$07, $02, $02, $0A
@@ -325,6 +343,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $02, $00, $1C
 
+; Bell Piano
 ;	Voice $12
 ;	$1C
 ;	$73, $72, $33, $32, 	$94, $99, $94, $99, 	$08, $0A, $08, $0A
@@ -343,6 +362,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $19, $00, $1E
 
+; Wet Wood Bass
 ;	Voice $13
 ;	$31
 ;	$33, $01, $00, $00, 	$9F, $1F, $1F, $1F, 	$0D, $0A, $0A, $0A
@@ -361,6 +381,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $1E, $1E, $1E
 
+; Silent Bass
 ;	Voice $14
 ;	$3A
 ;	$70, $76, $30, $71, 	$1F, $95, $1F, $1F, 	$0E, $0F, $05, $0C
@@ -379,6 +400,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $28, $12, $21
 
+; Picked Bass
 ;	Voice $15
 ;	$28
 ;	$71, $00, $30, $01, 	$1F, $1F, $1D, $1F, 	$13, $13, $06, $05
@@ -397,6 +419,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $1E, $14, $0E
 
+; Xylophone
 ;	Voice $16
 ;	$3E
 ;	$38, $01, $7A, $34, 	$59, $D9, $5F, $9C, 	$0F, $04, $0F, $0A
@@ -415,6 +438,7 @@
 	smpsVcReleaseRate   $06, $06, $0F, $0F
 	smpsVcTotalLevel    $00, $23, $00, $28
 
+; Sine Flute
 ;	Voice $17
 ;	$39
 ;	$32, $31, $72, $71, 	$1F, $1F, $1F, $1F, 	$00, $00, $00, $00
@@ -433,6 +457,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $28, $32, $1B
 
+; Pipe Organ
 ;	Voice $18
 ;	$07
 ;	$34, $74, $32, $71, 	$1F, $1F, $1F, $1F, 	$0A, $0A, $05, $03
@@ -451,6 +476,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $00, $0A, $0A
 
+; Synth Brass 2
 ;	Voice $19
 ;	$3A
 ;	$31, $37, $31, $31, 	$8D, $8D, $8E, $53, 	$0E, $0E, $0E, $03
@@ -469,6 +495,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $26, $28, $17
 
+; Harpischord
 ;	Voice $1A
 ;	$3B
 ;	$3A, $31, $71, $74, 	$DF, $1F, $1F, $DF, 	$00, $0A, $0A, $05
@@ -487,6 +514,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $0F, $1E, $32
 
+; Metallic Bass
 ;	Voice $1B
 ;	$05
 ;	$04, $01, $02, $04, 	$8D, $1F, $15, $52, 	$06, $00, $00, $04
@@ -505,6 +533,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $0C, $04, $10, $16
 
+; Alternate Metallic Bass
 ;	Voice $1C
 ;	$2C
 ;	$71, $74, $32, $32, 	$1F, $12, $1F, $12, 	$00, $0A, $00, $0A
@@ -523,6 +552,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $17, $00, $16
 
+; Backdropped Metallic Bass
 ;	Voice $1D
 ;	$3A
 ;	$01, $07, $01, $01, 	$8E, $8E, $8D, $53, 	$0E, $0E, $0E, $03
@@ -541,6 +571,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $0F, $27, $28, $18
 
+; Sine like Bell
 ;	Voice $1E
 ;	$36
 ;	$7A, $32, $51, $11, 	$1F, $1F, $59, $1C, 	$0A, $0D, $06, $0A
@@ -559,6 +590,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $01, $0B, $1E
 
+; Synth like Metallic with Small Bell
 ;	Voice $1F
 ;	$3C
 ;	$71, $72, $3F, $34, 	$8D, $52, $9F, $1F, 	$09, $00, $00, $0D
@@ -577,6 +609,7 @@
 	smpsVcReleaseRate   $07, $02, $08, $03
 	smpsVcTotalLevel    $0A, $1D, $05, $15
 
+; Nice Synth like lead
 ;	Voice $20
 ;	$3E
 ;	$77, $71, $32, $31, 	$1F, $1F, $1F, $1F, 	$0D, $06, $00, $00
@@ -595,6 +628,7 @@
 	smpsVcReleaseRate   $0A, $0A, $0A, $05
 	smpsVcTotalLevel    $0F, $0F, $0F, $1B
 
+; Rock Organ
 ;	Voice $21
 ;	$07
 ;	$34, $74, $32, $71, 	$1F, $1F, $1F, $1F, 	$0A, $0A, $05, $03
@@ -613,6 +647,7 @@
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $0A, $0A, $0A, $0A
 
+; Strike like Slap Bass
 ;	Voice $22
 ;	$20
 ;	$36, $35, $30, $31, 	$DF, $DF, $9F, $9F, 	$07, $06, $09, $06
