ebfc855a948d5c9ee50ec9d070965c1cd02af949
flamewing <flamewing.sonic@gmail.com>
2018-08-11 20:47:57 +0200

    Changing license to BSD 0-clause license.
    lt;drLegal: https://tldrlegal.com/license/bsd-0-clause-license

--- _smps2asm_inc.asm
+++ _smps2asm_inc.asm
@@ -1,6 +1,17 @@
 ; ===========================================================================
 ; Created by Flamewing, based on S1SMPS2ASM version 1.1 by Marc Gordon (AKA Cinossu)
 ; ===========================================================================
+; Permission to use, copy, modify, and/or distribute this software for any
+; purpose with or without fee is hereby granted.
+;
+; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+; WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+; MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+; ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+; WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+; OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+; ===========================================================================
 
 SMPS2ASMVer	= 1
 
