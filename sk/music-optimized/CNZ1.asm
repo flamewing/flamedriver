2879393d07c681ba7a5c9cd17c6b964e3324a0a7
Clownacy <Clownacy@users.noreply.github.com>
2021-04-27 13:52:23 +0100

    Fix more note-rest-duration-duration bugs
    These fix Carnival Night Zone 1 & 2, and the Sonic 3C credits music.
    
    CNZ is special, because its bugs aren't note-rest-duration-duration
    at all: they're just rest-duration-duration as they're the first
    bytes of the track, causing uninitialised notes to play.

--- sk/music-optimized/CNZ1.asm
+++ sk/music-optimized/CNZ1.asm
@@ -18,7 +18,7 @@ Snd_CNZ1_Header:
 Snd_CNZ1_FM1:
 	smpsModSet          $02, $01, $05, $04
 	smpsSetvoice        $03
-	dc.b	nRst, $60, smpsNoAttack, $60
+	dc.b	nRst, $60, smpsNoAttack, nRst
 	smpsCall            Snd_CNZ1_Call06
 	dc.b	nRst, $48
 
