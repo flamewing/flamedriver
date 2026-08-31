da7ff3b954361b7ab1364f345ffbf17a9c1f7e94
flamewing <flamewing.sonic@gmail.com>
2026-08-31 16:59:18 +0200

    Missed one song

--- s2/music-improved/9C - Continue.asm
+++ s2/music-improved/9C - Continue.asm
@@ -33,7 +33,6 @@ Continue_FM2:
 	smpsSetvoice        $01
 	smpsAlterVol        $02
 	smpsAlterPitch      $F4
-	smpsSetCommByte     $01
 	dc.b	nA5, $0C, nAb5, nG5, nFs5
 	smpsAlterVol        $FE
 	smpsAlterPitch      $0C
@@ -47,7 +46,6 @@ Continue_Loop03:
 	smpsAlterPitch      $FD
 	dc.b	nB4, $06, nRst, nB4, nRst, nFs4, nRst, nFs4, nRst, nE5, $0C, nRst
 	dc.b	$06, nEb5, $4E
-	smpsSetCommByte     $01
 	smpsStop
 
 ; FM3 Data
